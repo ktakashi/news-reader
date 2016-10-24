93ec5cee8198628d64ed670a6f9568564a5a6cc7
Takashi Kato <ktakashi@ymail.com>
2016-10-24 15:21:47 +0200

    Supporting language column thing

--- scripts/insert_data.sql
+++ scripts/insert_data.sql
@@ -2,7 +2,8 @@ insert into feed_type (id, name, plugin)
 values (1, 'RSS 2.0', '(news-reader rss2)');
 
 insert into languages (id, code3, code2, name)
-values (1,'aar','aa','Afar'),
+values (0,'xxx','','Unknown')
+       (1,'aar','aa','Afar'),
        (2,'abk','ab','Abkhazian'),
        (3,'ace','','Achinese'),
        (4,'ach','','Acoli'),
