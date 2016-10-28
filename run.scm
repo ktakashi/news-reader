84e356b9dc3c38c53823d77045ba3826a3c7891f
Takashi Kato <ktakashi@ymail.com>
2016-10-28 11:14:36 +0200

    Using . and absolute-path to detect root directory

--- run.scm
+++ run.scm
@@ -17,7 +17,7 @@
 
 
 (define *plato-roots* (make-parameter '()))
-(define-constant +default-plato-root+ "/home/takashi/projects/news-reader")
+(define-constant +default-plato-root+ (absolute-path "."))
 (define-constant +stop-commands+ '("stop" "restart"))
 
 ;; paella dispatcher
