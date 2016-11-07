3cff08c06f6677ed149e6b749b2f0282976fa2fd
Takashi Kato <ktakashi@ymail.com>
2016-11-07 17:55:10 +0100

    Adding unique constraint comment...

--- scripts/create_constraints.sql
+++ scripts/create_constraints.sql
@@ -4,6 +4,10 @@ alter table feed add constraint fk_feed_feed_type
 foreign key (feed_type_id) references feed_type(id);
 alter table feed add constraint fk_feed_languages
 foreign key (language_id) references languages(id);
+-- Some of titles are duplicated
+-- FIXME this is sort of needed...
+--alter table feed add constraint uk_feed_provider
+--unique (provider_id, title);
 
 alter table feed_summary add constraint fk_feed_summary_feed
 foreign key (feed_id) references feed(id);
