a6cc02aa085c7e1156b75819422abba881423fb6
Takashi Kato <ktakashi@ymail.com>
2016-11-22 16:16:22 +0100

    mobile support

--- apps/news/scripts/news.js
+++ apps/news/scripts/news.js
@@ -1,3 +1,10 @@
+// From http://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+function check_mobile() {
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+}
+
 angular.module('news', ['ngMaterial', 'ngSanitize'])
     .config(function ($provide, $httpProvider) {
 	$provide.factory('HttpInterceptor', function($q, $rootScope) {
@@ -32,7 +39,8 @@ angular.module('news', ['ngMaterial', 'ngSanitize'])
 	$scope.hide_provider = {};
 	$scope.offsets = {};
 	$scope.filters = {};
-
+	$scope.is_mobile = check_mobile();
+	
 	$http.get("/news/providers").
 	    then(function (response) {
 		$scope.providers = response.data;
@@ -58,23 +66,29 @@ angular.module('news', ['ngMaterial', 'ngSanitize'])
 		    });
 		});
 	};
-	$scope.show_link = function (ev, link, title) {
-	    $mdDialog.show({
-		templateUrl: '/news/html/iframe.html',
-		parent: angular.element(document.body),
-		targetEvent: ev,
-		clickOutsideToClose: true,
-		controller: 'iFrameCtrl',
-		fullscreen: true,
-		resolve: {
-		    url: function () {
-			return link;
-		    },
-		    title: function() {
-			return title;
+	if ($scope.is_mobile) {
+	    $scope.show_link = function (ev, link, title) {
+		window.open(link, '_blank');
+	    }
+	} else {
+	    $scope.show_link = function (ev, link, title) {
+		$mdDialog.show({
+		    templateUrl: '/news/html/iframe.html',
+		    parent: angular.element(document.body),
+		    targetEvent: ev,
+		    clickOutsideToClose: true,
+		    controller: 'iFrameCtrl',
+		    fullscreen: true,
+		    resolve: {
+			url: function () {
+			    return link;
+			},
+			title: function() {
+			    return title;
+			}
 		    }
-		}
-	    });
+		});
+	    }
 	};
 	$scope.load_summary = function(provider) {
 	    // only for UX, it's better to see something is working...
@@ -164,14 +178,58 @@ angular.module('news', ['ngMaterial', 'ngSanitize'])
 	}
     })
 
-    .directive('insertThing', function($http) {
-	return function(scope, element, attr) {
-	    if (!scope.provider) {
-		angular.element(element).removeClass("feed-contents");
-		angular.element(element).addClass("inserted");
+// Based on https://github.com/EricWVGG/AngularSlideables
+// License of slidable
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Eric Jacobsen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+    .directive('slideToggle', function() {
+	return {
+	    restrict: 'A',
+	    link: function(scope, element, attrs) {
+		var target, svg;
+		attrs.expanded = false;
+		element.bind('click', function() {
+		    console.log(target);
+		    if (!target) target = document.querySelector(attrs.slideToggle);
+		    // FIXME this is depending on AngularJS 
+		    if (!svg) svg = element.children();
+		    
+		    if(!attrs.expanded) {
+			target.style.height = '300px';
+		    } else {
+			target.style.height = '0px';
+		    }
+		    attrs.expanded = !attrs.expanded;
+		    if (attrs.expanded) {
+			svg.addClass('arrow-up');
+		    } else {
+			svg.removeClass('arrow-up');
+		    }
+		});
 	    }
-	};
-    });
+	}
+    })
+;
 
 function check_frame(iframe) {
 //    var doc = iframe.contentWindow;
